9709138aa2ee8d841e2e10dea0a5e53f44a6e0db
Prakash051196 <60376672+Prakash051196@users.noreply.github.com>
2020-01-29 13:54:40 +0530

    prakashloginTest

--- src/main/java/practice5/Testpractice5.java
+++ src/main/java/practice5/Testpractice5.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin={"json:target/cucumber.json"}
-		features = "Feature5"
+		,features = "Feature5"
 		,glue= {"step_defination5"}
 		)
 
